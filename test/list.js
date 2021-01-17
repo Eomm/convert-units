23db9ac64036767860e9df32497b3ca47bb713d1
Manuel Spigolon <behemoth89@gmail.com>
2021-01-17 11:58:53 +0100

    fix lint

--- test/list.js
+++ test/list.js
@@ -6,16 +6,18 @@ tests.list = function () {
   const list = convert().list()
   const firstItem = list[0]
 
+  const hasOwnProperty = Object.hasOwnProperty.bind(firstItem)
+
   assert(list.length > 0)
-  assert(firstItem.hasOwnProperty('abbr'))
+  assert(hasOwnProperty('abbr'))
   assert(typeof firstItem.abbr === 'string')
-  assert(firstItem.hasOwnProperty('measure'))
+  assert(hasOwnProperty('measure'))
   assert(typeof firstItem.measure === 'string')
-  assert(firstItem.hasOwnProperty('system'))
+  assert(hasOwnProperty('system'))
   assert(typeof firstItem.system === 'string')
-  assert(firstItem.hasOwnProperty('singular'))
+  assert(hasOwnProperty('singular'))
   assert(typeof firstItem.singular === 'string')
-  assert(firstItem.hasOwnProperty('plural'))
+  assert(hasOwnProperty('plural'))
   assert(typeof firstItem.plural === 'string')
 }
 
@@ -23,7 +25,7 @@ tests['list by measure'] = function () {
   const full = convert().list()
   const measures = convert().measures()
 
-  measures.map(function (measure) {
+  measures.forEach(function (measure) {
     const list = convert().list(measure)
 
     assert(list.length > 0)
