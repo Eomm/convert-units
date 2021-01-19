4438665a3d2361a958f143b9e54d1d4adebda12a
Manuel Spigolon <behemoth89@gmail.com>
2021-01-19 21:37:18 +0100

    feat: sec/mi (#2)
    * add sec/mi
    
    * fix test
    
    * fix conversion

--- test/pace.js
+++ test/pace.js
@@ -15,8 +15,7 @@ test('s/m to min/km', (t) => {
   t.plan(1)
   const expected = 16.6667
   const actual = convert(1).from('s/m').to('min/km')
-  t.ok(percentError(expected, actual) < ACCURACY
-    , 'Expected: ' + expected + ', Actual: ' + actual)
+  t.ok(percentError(expected, actual) < ACCURACY, `Expected: ${expected}, Actual: ${actual}`)
 })
 
 test('s/ft to s/ft', (t) => {
@@ -33,14 +32,31 @@ test('min/mi to min/km', (t) => {
   t.plan(1)
   const expected = 0.621371
   const actual = convert(1).from('min/mi').to('min/km')
-  t.ok(percentError(expected, actual) < ACCURACY
-    , 'Expected: ' + expected + ', Actual: ' + actual)
+  t.ok(percentError(expected, actual) < ACCURACY, `Expected: ${expected}, Actual: ${actual}`)
 })
 
 test('min/km to min/mi', (t) => {
   t.plan(1)
   const expected = 1.60934
   const actual = convert(1).from('min/km').to('min/mi')
-  t.ok(percentError(expected, actual) < ACCURACY
-    , 'Expected: ' + expected + ', Actual: ' + actual)
+  t.ok(percentError(expected, actual) < ACCURACY, `Expected: ${expected}, Actual: ${actual}`)
+})
+
+test('min/km to s/mi', (t) => {
+  t.plan(1)
+  const expected = 482.8032
+  const actual = convert(5).from('min/km').to('s/mi')
+  t.ok(percentError(expected, actual) < ACCURACY, `Expected: ${expected}, Actual: ${actual}`)
+})
+
+test('min/mi to s/mi', (t) => {
+  t.plan(1)
+  const expected = 60
+  const actual = convert(1).from('min/mi').to('s/mi')
+  t.ok(percentError(expected, actual) < ACCURACY, `Expected: ${expected}, Actual: ${actual}`)
+})
+
+test('s/ft to s/mi', (t) => {
+  t.plan(1)
+  t.equals(convert(1).from('s/ft').to('s/mi'), 5280)
 })
